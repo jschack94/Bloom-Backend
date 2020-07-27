3a60cfcc285e4a41b10ea839b94ff4199db26fa9
Jonathan <jschack94@gmail.com>
2020-07-27 13:28:45 -0500

    change to postgres

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2020_04_04_172125) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "connections", force: :cascade do |t|
     t.integer "mentee_id"
     t.integer "mentor_id"
